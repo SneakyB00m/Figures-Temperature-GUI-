f687d62a32d16d541606d1261926048964d4e2cf
Alan <86123617+SneakyB00m@users.noreply.github.com>
2022-07-22 00:16:04 -0500

    Add files via upload

---
+++ WForm Convert Temp-Fig/FiguresWF.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Degrees
+{
+    partial class FiguresWF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FiguresWF));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.userControl131 = new Degrees.UserControl13();
+            this.userControl121 = new Degrees.UserControl12();
+            this.userControl111 = new Degrees.UserControl11();
+            this.userControl101 = new Degrees.UserControl10();
+            this.userControl91 = new Degrees.UserControl9();
+            this.userControl81 = new Degrees.UserControl8();
+            this.userControl71 = new Degrees.UserControl7();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Degrees.Properties.Resources.Back2;
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 390);
+            this.panel1.TabIndex = 0;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Transparent;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.Crimson;
+            this.button10.Location = new System.Drawing.Point(14, 342);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(171, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Exit";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Cyan;
+            this.button8.Location = new System.Drawing.Point(14, 313);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(171, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Return";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.button9.Location = new System.Drawing.Point(14, 284);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(171, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Clear";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button7.Location = new System.Drawing.Point(12, 228);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(173, 30);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Scalene";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button6.Location = new System.Drawing.Point(12, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(173, 30);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Isosceles";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button5.Location = new System.Drawing.Point(12, 156);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(173, 30);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Equilateral";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button4.Location = new System.Drawing.Point(12, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 30);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Circle";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button3.Location = new System.Drawing.Point(12, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 30);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Rectangle";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button2.Location = new System.Drawing.Point(12, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Parallelogram";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Square";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::Degrees.Properties.Resources.Back2;
+            this.panel2.Controls.Add(this.userControl131);
+            this.panel2.Controls.Add(this.userControl121);
+            this.panel2.Controls.Add(this.userControl111);
+            this.panel2.Controls.Add(this.userControl101);
+            this.panel2.Controls.Add(this.userControl91);
+            this.panel2.Controls.Add(this.userControl81);
+            this.panel2.Controls.Add(this.userControl71);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 390);
+            this.panel2.TabIndex = 1;
+            // 
+            // userControl131
+            // 
+            this.userControl131.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl131.BackgroundImage")));
+            this.userControl131.Location = new System.Drawing.Point(3, 48);
+            this.userControl131.Name = "userControl131";
+            this.userControl131.Size = new System.Drawing.Size(375, 260);
+            this.userControl131.TabIndex = 6;
+            this.userControl131.Load += new System.EventHandler(this.userControl131_Load);
+            // 
+            // userControl121
+            // 
+            this.userControl121.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl121.BackgroundImage")));
+            this.userControl121.Location = new System.Drawing.Point(6, 48);
+            this.userControl121.Name = "userControl121";
+            this.userControl121.Size = new System.Drawing.Size(375, 260);
+            this.userControl121.TabIndex = 5;
+            this.userControl121.Load += new System.EventHandler(this.userControl121_Load);
+            // 
+            // userControl111
+            // 
+            this.userControl111.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl111.BackgroundImage")));
+            this.userControl111.Location = new System.Drawing.Point(6, 47);
+            this.userControl111.Name = "userControl111";
+            this.userControl111.Size = new System.Drawing.Size(375, 260);
+            this.userControl111.TabIndex = 4;
+            // 
+            // userControl101
+            // 
+            this.userControl101.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl101.BackgroundImage")));
+            this.userControl101.Location = new System.Drawing.Point(6, 47);
+            this.userControl101.Name = "userControl101";
+            this.userControl101.Size = new System.Drawing.Size(375, 260);
+            this.userControl101.TabIndex = 3;
+            // 
+            // userControl91
+            // 
+            this.userControl91.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl91.BackgroundImage")));
+            this.userControl91.Location = new System.Drawing.Point(6, 48);
+            this.userControl91.Name = "userControl91";
+            this.userControl91.Size = new System.Drawing.Size(375, 260);
+            this.userControl91.TabIndex = 2;
+            // 
+            // userControl81
+            // 
+            this.userControl81.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl81.BackgroundImage")));
+            this.userControl81.Location = new System.Drawing.Point(6, 47);
+            this.userControl81.Name = "userControl81";
+            this.userControl81.Size = new System.Drawing.Size(375, 260);
+            this.userControl81.TabIndex = 1;
+            // 
+            // userControl71
+            // 
+            this.userControl71.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControl71.BackgroundImage")));
+            this.userControl71.Location = new System.Drawing.Point(6, 47);
+            this.userControl71.Name = "userControl71";
+            this.userControl71.Size = new System.Drawing.Size(375, 260);
+            this.userControl71.TabIndex = 0;
+            // 
+            // FiguresWF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Degrees.Properties.Resources.Back2;
+            this.ClientSize = new System.Drawing.Size(800, 390);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FiguresWF";
+            this.Text = "FiguresWF";
+            this.Load += new System.EventHandler(this.FiguresWF_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel2;
+        private UserControl12 userControl121;
+        private UserControl11 userControl111;
+        private UserControl10 userControl101;
+        private UserControl9 userControl91;
+        private UserControl8 userControl81;
+        private UserControl7 userControl71;
+        private UserControl13 userControl131;
+    }
+}
---
+++ WForm Convert Temp-Fig/FiguresWF.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Degrees
+{
+    public partial class FiguresWF : Form
+    {
+        public FiguresWF()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Action<Control.ControlCollection> action = null;
+            action = (Controls) =>
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control is TextBox)
+                    {
+                        (control as TextBox).Clear();
+                    }
+                    else
+
+                        action(control.Controls);
+
+                }
+            };
+            action(Controls);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl71.Show();
+            userControl71.BringToFront();
+            //hide
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl81.Show();
+            userControl81.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl91.Show();
+            userControl91.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl101.Show();
+            userControl101.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FiguresWF_Load(object sender, EventArgs e)
+        {
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl111.Show();
+            userControl111.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl121.Hide();
+            userControl131.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl121.Show();
+            userControl121.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl131.Hide();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //show           
+            userControl131.Show();
+            userControl131.BringToFront();
+            //hide
+            userControl71.Hide();
+            userControl81.Hide();
+            userControl91.Hide();
+            userControl101.Hide();
+            userControl111.Hide();
+            userControl121.Hide();
+        }
+
+        private void userControl121_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl131_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
